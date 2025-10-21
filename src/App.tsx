048ace969b95ac17066d21d4b4fee1aaf228a21b
isantidev <santis.sierra6@gmail.com>
2025-10-20 23:08:24 -0500

    max width and content alignment fixed in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 
 function App() {
     return (
-        <main className="max-w-2xl">
+        <main className="max-w-7xl w-full px-4 mx-auto flex flex-col justify-center items-center">
             <h1 className="text-5xl font-extrabold">
                 ¡Encuentra tu proximo reto!
             </h1>
