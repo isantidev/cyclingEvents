fd45c4d53f0b7b9ac9c7279351fbc97de1d60573
isantidev <santis.sierra6@gmail.com>
2025-10-21 21:34:07 -0500

    Final component created

--- src/components/Hero/HeroBanner.tsx
+++ src/components/Hero/HeroBanner.tsx
@@ -24,7 +24,7 @@ const RaceCaption = ({
             >
                 <h3
                     aria-label="race-title"
-                    className="font-extrabold text-4xl md:text-5xl tracking-tight text-white"
+                    className="font-extrabold text-4xl md:text-5xl xl:text-6xl tracking-tight text-white"
                 >
                     {title}
                 </h3>
@@ -43,7 +43,7 @@ const RaceCaption = ({
 const HeroBanner = () => {
     return (
         <section
-            className="max-w-5xl w-full aspect-3/4 md:aspect-video bg-center bg-cover bg-no-repeat my-0 mx-auto overflow-hidden rounded-xl
+            className="max-w-6xl w-full aspect-3/4 md:aspect-video xl:aspect-2/1 bg-center bg-cover bg-no-repeat my-0 mx-auto overflow-hidden rounded-xl
             bg-[url(https://imgs.search.brave.com/OmSvzmQH1CIk1KjxWa6au7BmRqmXx_rRydK3Owh9IsY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zcGly/b2JpY3ljbGVzLmNv/bS9jZG4vc2hvcC9m/aWxlcy9qcGVnLW9w/dGltaXplcl9iaWNp/Y2xldGFfbXRiX291/dGRvb3JfLTEyLmpw/Zz9jcm9wPWNlbnRl/ciZoZWlnaHQ9MjQ4/OCZ2PTE3NTMwNTQ5/NTQmd2lkdGg9Mzcz/Mg)] 
             relative"
         >
