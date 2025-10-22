6b5b7411491f62e963779ef32e1991c60ba960ad
isantidev <santis.sierra6@gmail.com>
2025-10-21 19:01:48 -0500

    Refactor main, fixing max width and items alignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-// import HeroBanner from "./components/Hero/HeroBanner";
+import HeroBanner from "./components/Hero/HeroBanner";
 
 function App() {
     return (
         <main className="max-w-7xl w-full px-4 mx-auto flex flex-col justify-center items-center">
+            <HeroBanner />
             <h1 className="text-5xl font-extrabold">
                 ¡Encuentra tu proximo reto!
             </h1>
