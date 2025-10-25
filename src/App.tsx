6bbd030d687ae7e81de4024853486fa36fd6b888
isantidev <santis.sierra6@gmail.com>
2025-10-25 01:47:14 -0500

    feat: create new hook to fetch the Colombia data
    - fetch the list of departments
    - fetch the list of cities within a department using the department's is

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from "react";
 import HeroBanner from "./components/Hero/HeroBanner";
-import Races from "./components/layout/Races";
-import type { Race } from "./types/race";
-import { raceService } from "./services/raceService";
-
-const SHOWN_RACES_HOME = 6;
+// import Races from "./components/layout/Races";
 
 function App() {
-    const [races, setRaces] = useState<Race[]>([]);
-    useEffect(() => {
-        raceService.getByLimit(SHOWN_RACES_HOME).then(setRaces);
-    }, []);
-
     return (
         <main className="max-w-7xl w-full mx-auto px-4 my-8 flex flex-col gap-8 lg:gap-16 justify-center items-center">
             <HeroBanner />
             <h1 className="text-5xl font-extrabold">
                 ¡Encuentra tu proximo reto!
             </h1>
-            <Races races={races} />
         </main>
     );
 }
