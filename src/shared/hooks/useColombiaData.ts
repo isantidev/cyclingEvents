9ecac3eaeffb7edb4251b487569bb54ce9d0cfe9
isantidev <santis.sierra6@gmail.com>
2025-10-28 21:22:35 -0500

    Refactor: move department fetching logic to a separate file and clean up useColombiaData hook

--- src/shared/hooks/useColombiaData.ts
+++ src/shared/hooks/useColombiaData.ts
@@ -1,37 +1,4 @@
 import { useEffect, useState } from "react";
-import type { Department } from "@shared/types/colombiaData.types";
-
-const API_BASE_URL = "https://api-colombia.com/api/v1";
-
-// Fetch all deparments
-export async function fetchDepartments(): Promise<Department[]> {
-    const response = await fetch(`${API_BASE_URL}/Department`);
-    if (!response.ok) throw new Error("Failed to fetch departments");
-    return response.json();
-}
-
-export function useColombiaCitiesData(department_id: number) {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-
-    useEffect(() => {
-        if (!department_id) return;
-
-        fetch(`${API_BASE_URL}/Department/${department_id}/cities`)
-            .then((response) => response.json())
-            .then((cityInfo) => {
-                setData(cityInfo);
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                setError(error);
-                setIsLoading(false);
-            });
-    }, [department_id]);
-
-    return { citiesData: data, citiesError: error, citiesStatus: isLoading };
-}
 
 export function useColombiaCityData(city_id: number) {
     const [cityData, setCityData] = useState([]);
