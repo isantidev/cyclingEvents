e89973dd2b4cd8027e384b78ddd24ca9a779bdf0
isantidev <santis.sierra6@gmail.com>
2025-10-28 21:59:39 -0500

    Refactor: change useColombiaDepartments to an arrow function for consistency Add: create useColCitiesByDept hook for fetching cities by department

--- src/shared/hooks/useColCitiesByDept.ts
+++ src/shared/hooks/useColCitiesByDept.ts
@@ -1,23 +1,23 @@
 import { useEffect, useState } from "react";
-import { fetchDepartments } from "@shared/api/colombiaData";
-import type { Department } from "@shared/types/colombiaData.types";
+import { fetchCitiesByDepartment } from "@shared/api/colombiaData";
+import type { City } from "@shared/types/colombiaData.types";
 import type { FetchState } from "@shared/types/fetch.types";
 
-// Hook to fetch the info of all the departments
-export const useColombiaDepartments = () => {
-    const [state, setState] = useState<FetchState<Department[]>>({
+// Hook to fetch the cities' data by deparment
+export const useColCitiesByDept = (deparmentId: number) => {
+    const [state, setState] = useState<FetchState<City[]>>({
         data: null,
         error: null,
         isLoading: true,
     });
 
     useEffect(() => {
-        fetchDepartments()
+        fetchCitiesByDepartment(deparmentId)
             .then((data) => setState({ data, error: null, isLoading: false }))
             .catch((error) =>
                 setState({ data: null, error, isLoading: false })
             );
-    }, []);
+    }, [deparmentId]);
 
     return state;
 };
