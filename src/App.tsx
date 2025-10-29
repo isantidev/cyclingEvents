17d0d30d77b7793cee753547151510c2e2470df5
isantidev <santis.sierra6@gmail.com>
2025-10-29 18:27:24 -0500

    style: update main container styling in App component for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import HomePage from "@modules/web/pages/HomePage";
 
 function App() {
     return (
-        <main className="max-w-7xl w-full mx-auto px-4 my-8 flex flex-col gap-8 lg:gap-16 justify-center items-center">
+        <main className="max-w-[1600px] w-full mx-auto px-4 my-4 flex flex-col justify-center items-center">
             <HomePage />
         </main>
     );
