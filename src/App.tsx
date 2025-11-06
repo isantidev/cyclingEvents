67d66a5e6d827b2e4b443f3547c23a7a7e8a1994
isantidev <santis.sierra6@gmail.com>
2025-11-05 22:09:21 -0500

    refactor: update App component styling and enhance useRaces hook for improved race data processing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import HomePage from "@modules/web/pages/HomePage";
 
 function App() {
     return (
-        <main className="max-w-[1600px] w-full mx-auto px-4 my-4 flex flex-col justify-center items-center">
+        <main className="max-w-[1440px] w-full mx-auto px-4 my-4 flex flex-col justify-center items-center">
             <HomePage />
         </main>
     );
