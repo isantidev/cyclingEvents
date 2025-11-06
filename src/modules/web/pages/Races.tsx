9c638a542371b204c27c8826140f4d7ba1382e6c
isantidev <santis.sierra6@gmail.com>
2025-11-06 18:15:22 -0500

    Fix Card props passing

--- src/modules/web/pages/Races.tsx
+++ src/modules/web/pages/Races.tsx
@@ -1,12 +1,12 @@
 import Card from "@modules/web/components/Card/RaceCard";
 import { useRaces } from "@shared/hooks/useRaces";
-import type { RaceWithProcessedData } from "@shared/types/race.types";
+import type { Race } from "@shared/types/race.types";
 // import Search from "../Search/Search";
 
 const Races = () => {
     const { races, error, isLoading } = useRaces();
     if (error) console.error(error);
-    
+
     return (
         <>
             <search className="w-full">
@@ -20,18 +20,8 @@ const Races = () => {
             {error && <p>Ha ocurrido un problema al cargas las carreras...</p>}
             {races && !error && isLoading == false && (
                 <section className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-fr gap-8">
-                    {races.map((race: RaceWithProcessedData) => {
-                        return (
-                            <Card
-                                key={race.race_id}
-                                id={"race-card" + race.race_id}
-                                title={race.title}
-                                slug={race.slug}
-                                image_url={race.image_url}
-                                processedDate={race.processedDate}
-                                location={race.location}
-                            />
-                        );
+                    {races.map((race: Race) => {
+                        return <Card key={race.race_id} race_data={race} />;
                     })}
                 </section>
             )}
