7aeb042f73fe4c87c820d6cc4d1764bafb257f9a
isantidev <santis.sierra6@gmail.com>
2025-11-27 12:08:14 -0500

    feat: Implement 404 page and refactor routing with react-router dependency update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter } from "react-router";
 import HomePage from "@modules/web/pages/HomePage";
+import Navbar from "@modules/web/components/Nav/Navbar";
+import { webLinks } from "@shared/config/routes.config";
 
 function App() {
     return (
-        <main className="max-w-[1440px] w-full mx-auto px-4 my-4 flex flex-col justify-center items-center">
-            <HomePage />
-        </main>
+        <BrowserRouter>
+            <header className="sticky w-full top-0 z-900 flex justify-center items-center shadow-md bg-[#f5f7f8]">
+                <Navbar links={webLinks} />
+            </header>
+            <main className="max-w-[1440px] w-full mx-auto px-4 my-4 flex flex-col justify-center items-center">
+                <HomePage />
+            </main>
+        </BrowserRouter>
     );
 }
 
