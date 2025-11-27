a2f69526d6d5fbc3c23fbf1b5af4ad843a4d14c9
isantidev <santis.sierra6@gmail.com>
2025-11-27 12:28:10 -0500

    feat: implement client-side routing with React Router, add a dedicated races page and 404 route, and refactor the main content wrapper.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "@modules/web/pages/HomePage";
 import Navbar from "@modules/web/components/Nav/Navbar";
 import { webLinks } from "@shared/config/routes.config";
+import NotFound from "@shared/components/404";
+import Races from "@modules/web/pages/Races";
 
 function App() {
     return (
@@ -9,9 +11,13 @@ function App() {
             <header className="sticky w-full top-0 z-900 flex justify-center items-center shadow-md bg-[#f5f7f8]">
                 <Navbar links={webLinks} />
             </header>
-            <main className="max-w-[1440px] w-full mx-auto px-4 my-4 flex flex-col justify-center items-center">
-                <HomePage />
-            </main>
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/races" element={<Races />} />
+                {/* <Route path="/about" element={<HomePage />} /> */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </BrowserRouter>
     );
 }
--- src/modules/web/pages/HomePage.tsx
+++ src/modules/web/pages/HomePage.tsx
@@ -3,13 +3,15 @@ import FeaturedRaces from "@modules/web/components/FeaturedRaces/FeaturedRaces";
 
 const HomePage = () => {
     return (
-        <div className="w-full flex flex-col gap-8 lg:gap-16">
-            <HeroBanner />
-            <h1 className="text-5xl font-extrabold">
-                ¡Encuentra tu proximo reto!
-            </h1>
-            <FeaturedRaces />
-        </div>
+        <main className="max-w-[1440px] w-full mx-auto px-4 my-4 flex flex-col justify-center items-center">
+            <div className="w-full flex flex-col gap-8 lg:gap-16">
+                <HeroBanner />
+                <h1 className="text-5xl font-extrabold">
+                    ¡Encuentra tu proximo reto!
+                </h1>
+                <FeaturedRaces />
+            </div>
+        </main>
     );
 };
 
